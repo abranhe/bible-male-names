40e2cfddf68e5c8d09b296749496fe07276265e0
Carlos Abraham <abraham@abranhe.com>
2018-07-28 02:49:44 -0400

    jump to meow 5.0.0 and add who-was flag

--- cli.js
+++ cli.js
@@ -1,21 +1,32 @@
 #!/usr/bin/env node
 'use strict';
-var meow = require('meow');
-var bibleMaleNames = require('./');
+const meow = require('meow');
+const open = require('opn');
+const bibleMaleNames = require('./');
 
-var cli = meow([
-	'Examples',
-	'  $ bible-male-names',
-	'  Abraham',
-	'',
-	'  $ bible-male-names --all',
-	'  Aaron',
-	'  Abagtha',
-	'  Abda',
-	'  ...',
-	'',
-	'Options',
-	'  --all   Get all names'
-]);
+const search = 'https://www.google.com/search?q=';
 
-console.log(cli.flags.all ? bibleMaleNames.all.join('\n') : bibleMaleNames.rand());
+const cli = meow(`
+	Examples
+		$ bible-male-names
+		Abraham
+
+		$ bible-male-names --all
+		Aaron
+		Abagtha
+		Abda
+		...
+
+		Options
+			--all      Get all names
+			--who-was  Search on Google for the name
+`);
+
+let name = bibleMaleNames.rand();
+
+console.log(cli.flags.all ? bibleMaleNames.all.join('\n') : name);
+
+if(cli.flags.whoWas){
+	open(search + 'Who+was+' + name + '+in+The+Bible')
+	process.exit();
+}
